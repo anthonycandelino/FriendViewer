d1fed6e6e4b7aae1ea8610f7692417d4d7c742b6
Anthony Candelino <anthcandelino@gmail.com>
2024-09-07 17:19:04 -0400

    Initial Commit

---
+++ FriendViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FriendViewer
+//
+//  Created by Anthony Candelino on 2024-09-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FriendViewer/FriendViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendViewerApp.swift
+//  FriendViewer
+//
+//  Created by Anthony Candelino on 2024-09-07.
+//
+
+import SwiftUI
+
+@main
+struct FriendViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
